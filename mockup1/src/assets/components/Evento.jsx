01f70c70721af6276756c94a3a8ec21fcca1789e
Froddes <froddes.dev@gmail.com>
2025-12-29 19:20:33 +0100

    update mockup1

--- mockup1/src/assets/components/Evento.jsx
+++ mockup1/src/assets/components/Evento.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-
 import { FaRegHeart, FaRegClock, FaRegMap } from 'react-icons/fa';
+
+import Slider from './evento/Slider.jsx';
 
 import './../css/Evento.css';
 
@@ -48,6 +47,18 @@ function Evento() {
                     </div>
                 </div>
             </div>
+            <div className="map-wrapper" style={{ margin: '0 0 50px 0'}}>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3170.1915995405!2d-5.993439023455982!3d37.38350033446868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126c103398934b%3A0x679693959c8849b!2sReal%20Alc%C3%A1zar%20de%20Sevilla!5e0!3m2!1ses!2ses!4v1700000000000!5m2!1ses!2ses"
+                    width="1000"
+                    height="450"
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    title="Mapa Real Alcázar"
+                ></iframe>
+            </div>
+            <Slider />
         </div >
     )
 }
---
+++ mockup1/src/assets/components/evento/Slider.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+// Importamos los componentes de Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Importamos los estilos base y de módulos
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// Importamos los módulos que queremos usar
+import { Navigation, Pagination, Autoplay, EffectFade } from 'swiper/modules';
+
+function Slider() {
+  const fotos = [
+    '/fotos/alcazar1.jpg',
+    '/fotos/alcazar2.jpg',
+    '/fotos/alcazar3.jpg',
+    '/fotos/alcazar4.webp'
+  ];
+
+  return (
+    <div className="slider-container" style={{ padding: '40px 0', width: '100%' }}>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1} // Por defecto 1 en móvil
+        navigation
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 2000, disableOnInteraction: false }}
+        loop={true} // Infinito
+        breakpoints={{
+          // Cuando la pantalla sea de escritorio (768px o más)
+          768: {
+            slidesPerView: 2, // Muestra 2 fotos a la vez
+          },
+          1024: {
+            slidesPerView: 3, // Muestra 3 fotos a la vez
+          }
+        }}
+        style={{
+            "--swiper-navigation-color": "#c78665",
+            "--swiper-pagination-color": "#c78665",
+        }}
+      >
+        {fotos.map((src, index) => (
+          <SwiperSlide key={index}>
+            <div style={{  overflow: 'hidden', height: '450px' }}>
+              <img 
+                src={src} 
+                alt={`Alcázar ${index + 1}`} 
+                style={{ 
+                  width: '100%', 
+                  height: '100%', 
+                  objectFit: 'cover',
+                  display: 'block'
+                }} 
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Slider;
